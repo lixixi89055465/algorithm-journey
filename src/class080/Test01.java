f7965b1c2b0275712e0dd41c536cbcefab73fc0e
lixixi89055465 <1850094299@qq.com>
2025-11-09 08:17:01 +0800

    test

---
+++ src/class080/Test01.java
@@ -0,0 +1,21 @@
+package class080;
+
+/**
+ * 创建人: @author lixiang
+ * 创建时间: 2025年11月09日 08:15
+ * 项目名称: algorithm-journey
+ * 文件名称: Test01
+ * 文件描述: @Description:
+ * site:
+ *
+ // 我能赢吗
+ // 给定两个整数n和m
+ // 两个玩家可以轮流从公共整数池中抽取从1到n的整数（不放回）
+ // 抽取的整数会累加起来（两个玩家都算）
+ // 谁在自己的回合让累加和 >= m，谁获胜
+ // 若先出手的玩家能稳赢则返回true，否则返回false
+ // 假设两位玩家游戏时都绝顶聪明，可以全盘为自己打算
+ // 测试链接 : https://leetcode.cn/problems/can-i-win/
+ */
+public class Test01 {
+}
